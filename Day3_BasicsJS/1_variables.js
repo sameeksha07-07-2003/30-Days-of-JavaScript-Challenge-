fd2600c264b5ce9132eb03c410d3d562dd9e3060
sameeksha07-07-2003 <100752202+sameeksha07-07-2003@users.noreply.github.com>
2025-10-08 06:41:34 +0000

    Day3 Variables

---
+++ Day3_BasicsJS/1_variables.js
@@ -0,0 +1,38 @@
+//var let and const
+
+//const
+// const name = "Sameeksha"
+// console.log(name)
+
+//let 
+// let name = "Sameeksha"
+// name = "Yash"
+// console.log(name);
+
+//var
+// var name = "Sameeksha"
+// name = "Yash"
+// var name = "Ram"
+// console.log(name);
+
+// const name = "Sameeksha"
+// {
+//     const age = 10
+//     console.log(age);
+    
+// }
+// let const = 34;
+
+// console.log(name);
+
+//table
+let name = "Sameeksha"
+let age = 21
+let subject = "OS"
+location  = "Bhopal"
+
+let food ;
+
+console.log(location);
+console.table([name, age, subject, location , food])
+
