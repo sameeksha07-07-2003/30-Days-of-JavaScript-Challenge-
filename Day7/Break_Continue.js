7decce8a84ebc1222c999a68eadbbeee5c1fc58f
sameeksha07-07-2003 <100752202+sameeksha07-07-2003@users.noreply.github.com>
2025-10-13 11:20:29 +0000

    Day7 Loops,Break& continue

---
+++ Day7/Break_Continue.js
@@ -0,0 +1,19 @@
+
+//break
+// let i = 1;
+// while(i<6){
+//     console.log(i)
+//     if(i == 3)break;
+//     i++;
+// }
+
+//continue
+
+let i = 0;
+while(i<10){
+    
+    i++;
+    if(i == 7)continue;
+    console.log(i)
+    
+}
---
+++ Day7/Loops.js
@@ -0,0 +1,89 @@
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+
+//1. for loop
+// for (initialization; condition; increment/decrement) {
+//     // code block
+// }
+
+// for(let i = 1; i<11; i++ ){
+//    console.log("hello") 
+// }
+
+//2. while loop
+// while(condition){
+//     //code block
+    //increment/decrement
+// }
+
+// let i = 1;
+// while(i<6){
+//     console.log("hello")
+//     i++;
+// }
+
+//3. do while
+// do {
+//     // code block
+// } while (condition);
+
+// let i = 1;
+// do{
+//     console.log("hello")
+//     i++;
+// }while(i<1)
+
+//4. for in
+// for(key in object ){
+//     //code using object[key]
+// }
+
+// let user = {
+//     name : "Yash",
+//     age : 19 ,
+//     Gender: "Male",
+//     IsValid : true
+// }
+
+// for(let key in user){
+//     console.log("key is "+ key + " and value is " + user[key])
+// }
+
+// let fruits = ["Mango", "apple","banana"]
+
+// for(let fruit in fruits ){
+//     console.log(fruits[fruit])
+// }
+
+
+
+//for of loop
+// for(element of iterable){
+//     //code 
+// }
+
+// let fruits = ["Mango", "apple","banana"]
+
+// for(let fruit of fruits ){
+//     console.log(fruit)
+// }
+
+// let str = "Sameeksha"
+// for(let char of str){
+//     console.log(char)
+// }
+
+// let user = {
+//     name : "Yash",
+//     age : 19 ,
+//     Gender: "Male",
+//     IsValid : true
+// }
+
+// for(let elm of user){
+//     console.log(elm)
+// }
+//not work because user is not iterable 
